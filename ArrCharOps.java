0703b8706abe5adc1b648196a7f10246dc0d1005
Yael Turgeman <yael.turgeman@post.runi.ac.il>
2024-12-02 14:58:34 +0200

    done Arr

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -162,7 +162,7 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        if (str1 == null || str2 == null)
+        if (str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0 )
         {
             return -2;
         }
